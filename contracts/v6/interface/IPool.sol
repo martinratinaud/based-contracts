78812ae17e2eea5a9f0dfa308bff45f7cf4cfc96
Martin Ratinaud <martinratinaud@gmail.com>
2024-06-08 14:45:00 +0400

    Add NatSpec documentation to IPool interface
    - Document all interface functions
    - Add parameter and return value documentation
    - Improves integration clarity

--- contracts/v6/interface/IPool.sol
+++ contracts/v6/interface/IPool.sol
@@ -1,8 +1,18 @@
 //SPDX-License-Identifier: Beerware
 pragma solidity >=0.6.0;
 
+/// @title IPool Interface
+/// @notice Interface for staking pool contracts
 interface IPool {
+    /// @notice Claim accumulated rewards
     function getReward() external;
+
+    /// @notice Stake tokens into the pool
+    /// @param amount Amount of tokens to stake
     function stake(uint256 amount) external;
+
+    /// @notice View earned rewards for an account
+    /// @param account Address to check earned rewards for
+    /// @return Amount of rewards earned
     function earned(address account) external view returns (uint256);
 }
