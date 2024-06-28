70e2a31455103d06d8b681a46d23a575c599b2e9
Martin Ratinaud <martinratinaud@gmail.com>
2024-06-28 11:30:00 +0400

    Add comprehensive NatSpec documentation to RoverVault
    - Document contract purpose and functionality
    - Add parameter documentation for constructor
    - Clarify usage compared to FarmingRover

--- contracts/v6/RoverVault.sol
+++ contracts/v6/RoverVault.sol
@@ -3,12 +3,21 @@ pragma solidity >=0.6.0;
 
 import "./Rover.sol";
 
+/// @title RoverVault
+/// @notice Simple vault for converting reward tokens to BASED without farming
+/// @dev Simplified version of Rover that doesn't interact with reward pools
 contract RoverVault is Rover {
+    /// @notice Creates a new RoverVault instance
+    /// @param _based Address of the BASED token
+    /// @param _rewardToken Address of the reward token to convert
+    /// @param _pair Trading pair to use: either "sUSD" or "WETH"
     constructor(address _based, address _rewardToken, string memory _pair)
         public
         Rover(_based, _rewardToken, _pair)
     {}
 
+    /// @notice Initializes the rover and begins operations
+    /// @dev Can only be called once by the owner
     function startRover() public onlyOwner {
         init();
     }
